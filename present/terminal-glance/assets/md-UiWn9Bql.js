4db9c9371c8a4b8c5befd40610babbcea492ff60
zthxxx <zthxxx@users.noreply.github.com>
2025-02-20 01:54:43 +0000

    deploy: 53761ac9d12a58b5d0f568c3bdc9b08f38f5c329

---
+++ present/terminal-glance/assets/md-UiWn9Bql.js
@@ -0,0 +1,16 @@
+import{_ as r}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-KDWW_zmG.js";import{b as o,o as c,w as l,g as s,e as u,m as d,ac as n,v as m,x as _,C as e}from"./modules/vue-DUljWuK3.js";import{I as f}from"./default-B_qnSmTv.js";import{u as k,f as g}from"./slidev/context-DgeEfrJG.js";import{_ as v}from"./index-DWRn47cG.js";import"./modules/unplugin-icons-C7kWizq_.js";import"./modules/shiki-G5TKQc2E.js";const x={__name:"slides.md__slidev_15",setup(h){const{$clicksContext:p,$frontmatter:t}=k();return p.setup(),(T,a)=>{const i=r;return c(),o(f,m(_(e(g)(e(t),14))),{default:l(()=>[a[1]||(a[1]=s("p",null,"TTY 负责控制字符的输入输出",-1)),u(i,d({},{ranges:[]}),{default:l(()=>a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"                      +----------------+")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                      |   TTY Driver   |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                      |      I/O       |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                      |   +-------+    |       +----------------+")]),n(`
+`),s("span",{class:"line"},[s("span",null," +------------+       |   |       |<---------->| User process A |")]),n(`
+`),s("span",{class:"line"},[s("span",null," | Terminal A |<--------->| tty/0 |    |       +----------------+")]),n(`
+`),s("span",{class:"line"},[s("span",null," +------------+       |   |       |<---------->| User process B |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                      |   +-------+    |       +----------------+")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                      |                |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                      |   +-------+    |       +----------------+")]),n(`
+`),s("span",{class:"line"},[s("span",null," +------------+       |   |       |<---------->| User process C |")]),n(`
+`),s("span",{class:"line"},[s("span",null," | Terminal B |<--------->| tty/1 |    |       +----------------+")]),n(`
+`),s("span",{class:"line"},[s("span",null," +------------+       |   |       |<---------->| User process D |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                      |   +-------+    |       +----------------+")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                      |                |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                      +----------------+")])])],-1)])),_:1},16)]),_:1},16)}}},N=v(x,[["__scopeId","data-v-2d0225c1"]]);export{N as default};
